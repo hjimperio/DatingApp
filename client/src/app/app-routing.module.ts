7458ad79414fc9dbd8705aa0553a49ebaadeca56
Harold James Imperio <thedragon128@gmail.com>
2021-02-16 04:13:26 +0800

    End of Section 15

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -13,6 +13,7 @@ import { AiListComponent } from './tracker/ai-list/ai-list.component';
 import { AiRequestComponent } from './tracker/ai-request/ai-request.component';
 import { AuthGuard } from './_guards/auth.guard';
 import { PreventUnsavedChangesGuard } from './_guards/prevent-unsaved-changes.guard';
+import { MemberDetailedResolver } from './_resolvers/member-detailed.resolver';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -22,7 +23,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       {path: 'members', component: MemberListComponent},
-      {path: 'members/:username', component: MemberDetailComponent},
+      {path: 'members/:username', component: MemberDetailComponent, resolve: {member: MemberDetailedResolver}},
       {path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard]},
       {path: 'lists', component: ListsComponent},
       {path: 'messages', component: MessagesComponent},
